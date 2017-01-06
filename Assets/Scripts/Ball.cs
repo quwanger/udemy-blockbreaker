ce7c1efe9cb234f88d9282f8b459e015cc90bf34
QUANG-PC\quang <quwanger@gmail.com>
2017-01-05 19:26:14 -0500

    Got levels and menu flow in

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+	private Paddle paddle;
+
+	private bool hasStarted = false;
+	private Vector3 paddleToBallVector;
+
+	// Use this for initialization
+	void Start () {
+		paddle = GameObject.FindObjectOfType<Paddle>();
+
+		paddleToBallVector = this.transform.position - paddle.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!hasStarted)
+		{
+			// Lock ball to paddle
+			this.transform.position = paddle.transform.position + paddleToBallVector;
+
+			// Wait for a mouse press to launch
+			if (Input.GetMouseButtonDown(0))
+			{
+				print("Mouse clicked, launch ball");
+				hasStarted = true;
+				this.GetComponent<Rigidbody2D>().velocity = new Vector2(2.0f, 10.0f);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseCollider : MonoBehaviour {
+	private LevelManager levelManager;
+
+	private void OnTriggerEnter2D(Collider2D trigger)
+	{
+		levelManager = GameObject.FindObjectOfType<LevelManager>();
+
+		print("Trigger");
+		levelManager.LoadLevel("Win Screen");
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		print("Collision");
+	}
+}
